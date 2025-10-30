1532290f013a2e7557b76d33fb10e85b3a7b498a
zabronax <zabronax@gmail.com>
2025-10-30 10:12:31 +0100

    outline what we want

--- Program.cs
+++ Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿var consoleApp = new ConsoleApp();
+
+consoleApp.Command("", () => Console.WriteLine("Standard"));
+consoleApp.Command("hello", () => Console.WriteLine("Hello"));
+consoleApp.Command("bye", () => Console.WriteLine("Bye"));
+
+consoleApp.Run();
